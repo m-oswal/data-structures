aba1dc3a9c64ff46e6cfade2504ecd133a2f0c63
m-oswal <110195478+m-oswal@users.noreply.github.com>
2023-10-23 00:54:05 +0530

    sorting by insertion sort

---
+++ sorting/insertion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void insert_sort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {5, 3, 2, 1, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    insert_sort(arr, n);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
